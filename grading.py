aa9b638e4aad067c2293fbfff0ad47899e410c5a
Xin Yin <killkeeper@gmail.com>
2011-10-03 17:47:11 -0500

    Added support for bonus/penalty points/percentage with command mode; Fixed a bug that crashes program when pressing 0 in name search mode.

--- grading.py
+++ grading.py
@@ -8,9 +8,13 @@ import readline
 import cPickle as pickle
 from math import ceil
 
-MODE_RUBRIC, MODE_NAME, MODE_GRADE = 1,2,3
-COLOR_PAIR_PROMPT, COLOR_PAIR_NAME = 1,2
+MODE_RUBRIC, MODE_NAME, MODE_GRADE, MODE_COMMAND = 1,2,3, 4
+COLOR_PAIR_PROMPT, COLOR_PAIR_NAME, COLOR_PAIR_CMD = 1,2,3
 ROW_NAME, ROW_RUBRIC, ROW_GRADE, ROW_LIST = 0,1,2,3
+
+BONUS_PENALTY_CMDS = (ord('+'), ord('-'))
+
+REGEX_BNP = re.compile(r'^([+|-])(\d+)(\%?)$')
 
 class Grading:
     def __init__(self, subject):
@@ -22,6 +26,7 @@ class Grading:
         self.name_offset = len('NAME: ') - 1
         self.mode = MODE_RUBRIC
         self.selected_index = -1
+        self.bonus_penalty = []
 
         self.matched_indices = []
         self.load_roster()
@@ -33,6 +38,7 @@ class Grading:
             self.get_rubric()
         self.init_screen()
         self.show_rubric()
+        self.max_score = sum(self.rubric)
         if self.records:
             self.show_status(
                 '[CACHE] %d entries recovered from local cache. (!! to swipe)' % \
@@ -43,8 +49,12 @@ class Grading:
     def init_screen(self):
         self.stdscr = curses.initscr()
         curses.start_color()
-        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(COLOR_PAIR_PROMPT,
+                curses.COLOR_RED, curses.COLOR_WHITE)
+        curses.init_pair(COLOR_PAIR_NAME, 
+                curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(COLOR_PAIR_CMD, 
+                curses.COLOR_GREEN, curses.COLOR_WHITE)
         curses.noecho()
         curses.cbreak()
         self.stdscr.keypad(1)
@@ -96,7 +106,8 @@ class Grading:
                 grades = map(float, 
                         ''.join(self.buffer).strip().split(' '))
                 if len(grades) == len(self.rubric):
-                    total_grade = ceil(sum(grades))
+                    total_grade = self.real_score(self.selected_index, 
+                            sum(grades))
                     max_grade = sum(self.rubric)
 
                     self.show_grade(color=COLOR_PAIR_PROMPT)
@@ -113,12 +124,12 @@ class Grading:
         self.parse_grade()
         self.show_grade()
 
-    def show_status(self, status):
+    def show_status(self, status, color=COLOR_PAIR_NAME):
         yx = self.stdscr.getyx()
 
         self.clear_lines(self.ROW_MAX, 1)
         self.stdscr.addstr(self.ROW_MAX, 0, status,
-            curses.color_pair(COLOR_PAIR_NAME))
+            curses.color_pair(color))
 
         self.stdscr.move(*yx)
 
@@ -132,7 +143,8 @@ class Grading:
 
             self.show_status("[RECORDED] %s: %d/%d // [%d/%d]" % (
                 self.namelist[self.selected_index],
-                ceil(sum(grades)), sum(self.rubric),
+                self.real_score(self.selected_index, sum(grades)), 
+                sum(self.rubric),
                 len(self.records), len(self.namelist)))
 
     def name_keypress(self, raw_ch):
@@ -151,7 +163,7 @@ class Grading:
                 self.stdscr.addch(raw_ch)
 
             elif (ch == '\n' and self.matched_indices) or \
-                    (ch.isdigit() and \
+                    (ch.isdigit() and self.matched_indices and \
                     int(ch) <= len(self.matched_indices)):
 
                 # If user presses enter or specify a number,
@@ -189,28 +201,94 @@ class Grading:
                 self.destroy_screen()
                 sys.exit(0)
 
-            if raw_ch == ord('!'):
-                if not self.command or self.command[-1] == '!':
-                    self.command.append('!')
-                    self.show_status('[COMMAND]: %s' % \
-                            ''.join(self.command))
-                    if ''.join(self.command) == '!!':
-                        self.rubric = []
-                        self.records = []
-                        self.remain_indices = range(len(self.namelist))
-                        self.destroy_screen()
-                        os.remove('.%s.pickle' % self.subject)
-                        print '> CACHE SWIPED!! Please start over again!'
-                        sys.exit(0)
-                else:
-                    self.command = []
+            if raw_ch == ord(':') and not self.command and \
+                    not self.mode == MODE_COMMAND:
+                # Push current mode into stack
+                self.command.append(self.mode)
+                self.mode = MODE_COMMAND
+                self.command.append(':')
+
+                self.show_status(''.join(self.command[1:]), 
+                        color=COLOR_PAIR_CMD)
 
                 continue
+
+            if self.mode == MODE_COMMAND:
+                if raw_ch == curses.KEY_BACKSPACE and \
+                        len(self.command) > 1:
+                    # [prev_mode, ':']
+                    self.command.pop()
+
+                    if len(self.command) == 1:
+                        self.exec_command()
+                        continue
+
+                if raw_ch == ord('\n'):
+                    self.exec_command()
+                    continue
+
+                if raw_ch > 31 and raw_ch < 127:
+                    self.command.append(chr(raw_ch))
+
+                self.show_status(''.join(self.command[1:]), 
+                        color=COLOR_PAIR_CMD)
 
             if self.mode == MODE_NAME:
                 self.name_keypress(raw_ch)
             elif self.mode == MODE_GRADE:
                 self.grade_keypress(raw_ch)
+
+    def exec_command(self):
+        if not self.command:
+            return
+
+        # Recover previous mode
+        if len(self.command) == 1:
+            self.mode = self.command.pop()
+
+        if len(self.command) == 2:
+            # prev_mode, :
+            self.mode = self.command[0]
+            self.command = []
+            self.show_status('')
+            return
+
+        if len(self.command) > 2:
+            usr_cmd = ''.join(self.command[2:])
+
+        if usr_cmd == '!!':
+            self.rubric = []
+            self.records = []
+            self.remain_indices = range(len(self.namelist))
+            self.destroy_screen()
+            os.remove('.%s.pickle' % self.subject)
+            print '> CACHE SWIPED!! Please start over again!'
+            sys.exit(0)
+
+        if REGEX_BNP.match(''.join(usr_cmd)) and \
+                self.selected_index != -1 and \
+                self.command[0] == MODE_GRADE:
+            match = REGEX_BNP.match(usr_cmd)
+            sign, num, pct_flag = match.groups()
+            if pct_flag == '%':
+                self.bonus_penalty[self.selected_index] = 1 + [-1,1][sign=='+'] * float(num)/100
+            else:
+                self.bonus_penalty[self.selected_index] = [-1,1][sign=='+'] * int(num)
+
+            self.show_status('[%s] %s / %s' % (
+                ['PENALTY', 'BONUS'][sign=='+'],
+                match.group(),
+                self.namelist[self.selected_index]
+                ))
+            self.mode = self.command[0]
+            self.command = []
+            return
+
+        # None matches?
+        # Invalid command
+        self.mode == self.command[0]
+        self.command = []
+        self.show_status('')
 
     def show_rubric(self):
         padded_rubric = [str(self.rubric[x]).ljust(self.grade_spaces[x])
@@ -221,6 +299,14 @@ class Grading:
             curses.color_pair(COLOR_PAIR_PROMPT))
 
     def show_grade(self, color=0):
+        bp = self.bonus_penalty[self.selected_index]
+        bp_disp = ''
+        if bp != 0:
+            if type(bp) is float:
+                bp_disp = str(bp*100) + '%'
+            elif type(bp) is int:
+                bp_disp = ['','+'][bp>0] + str(bp) 
+
         grades = ''.join(self.buffer).split(' ')
         if grades:
             padded_grades = [grades[x].ljust(self.grade_spaces[x])
@@ -228,6 +314,8 @@ class Grading:
             padded_grades.append(grades[-1])
 
         self.clear_lines(ROW_GRADE, 1)
+        self.stdscr.addstr(ROW_GRADE, sum(self.grade_spaces)+8,
+                bp_disp)
         self.stdscr.addstr(ROW_GRADE, 0, 
             'GRADE:  ' + ''.join(padded_grades),
             curses.color_pair(color))
@@ -368,7 +456,9 @@ class Grading:
     def load_cache(self):
         if self.subject and os.path.exists('.%s.pickle' % self.subject):
             fd = open('.%s.pickle' % self.subject, 'r')
-            self.rubric, self.remain_indices, self.records = pickle.load(fd)
+            self.rubric, self.remain_indices, \
+                self.records, self.bonus_penalty = pickle.load(fd)
+
             self.grade_spaces = map(
                     lambda x: len(str(x)) + 3,
                     self.rubric)
@@ -378,10 +468,11 @@ class Grading:
             fd.close()
 
     def cache(self, flush=False):
-        if len(self.records) % 5 == 0 or flush:
+        if len(self.records) % 2 == 0 or flush:
             fd = open('.%s.pickle' % self.subject, 'w')
             pickle.dump(
-                (self.rubric, self.remain_indices, self.records),
+                (self.rubric, self.remain_indices, 
+                    self.records, self.bonus_penalty),
                 fd)
             fd.close()
 
@@ -391,21 +482,44 @@ class Grading:
             self.roster = [entry.strip().split('\t') for entry in entries]
             self.namelist = [r[2] for r in self.roster]
             self.remain_indices = set(range(len(self.namelist)))
+            self.bonus_penalty = [0] * len(self.namelist)
             self.num_students = len(self.roster)
         else:
             self.roster = None
 
+    def real_score(self, idx, score):
+        if idx >= len(self.bonus_penalty):
+            return score
+        bp = self.bonus_penalty[idx] 
+        if type(bp) is float:
+            score *= bp
+        elif type(bp) is int:
+            score += bp
+
+        score = ceil(score)
+        score = self.max_score if score > self.max_score else score
+        score = 0 if score < 0 else score
+
+        return score
+
     def save(self):
+        raw_data = [self.roster[r[0]] + r[1:] + \
+            [self.bonus_penalty[r[0]]] + \
+            [sum(r[1:])]
+            for r in self.records]
+
+        for d in raw_data:
+            d[-1] = self.real_score(int(d[0]), d[-1])
+
         merged_data = [','.join(map(lambda x: str(x) \
                 if type(x) in (float, int) \
                 or (type(x) is str and x.isdigit())\
                 else '"%s"' % x.strip(), 
-                self.roster[r[0]] + r[1:] + \
-                [ceil(sum(r[1:]))]))
-                for r in self.records]
+                r))
+                for r in raw_data]
 
         q_title = ['"q%d"' % x for x in range(1, self.num_questions+1)] \
-                + ['"total"']
+                + ['"bop"', '"total"']
 
         fd = open('%s.csv' % self.subject, 'w+')
         fd.write('"id","section","name","major","comajor","year","credit",')
